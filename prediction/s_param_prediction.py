4cf9dfc025e241c1c89bd75145ec0a0845d923dc
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-12 01:09:39 +0300

    s_param script update

--- prediction/s_param_prediction.py
+++ prediction/s_param_prediction.py
@@ -1,5 +1,5 @@
 import torch
-from load_set import create_s_param_prediction_arrays
+from load_set import create_s_param_prediction_arrays, create_s_param_dataloader
 
 # ============================================= Initializing Dataset ============================================= #
 torch.manual_seed(42)
@@ -23,5 +23,16 @@ batch_size_dict = {
 
 dataloader_dict = {}
 
+for test_name, test_info in test_info_dict.items():
+        x_array, y_array, _, _, thresholds, _ = test_info
+        batch_size = batch_size_dict[test_name]
 
-
+        dataloader_dict[test_name] = create_s_param_dataloader(
+            x_array,
+            y_array,
+            batch_size=batch_size,
+            seed=42,
+            standard_scale=True,
+            split_method="lhs"
+        )
+        
