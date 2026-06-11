9ed899bc05d698799c06590973abb6cea04874ea
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-11 21:58:27 +0300

    s_param predictor model

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -51,7 +51,49 @@ class DeepBERPredictor(nn.Module):
             x = layer(x) # pass through each layer
         output = self.output_layer(x) # final output layer
         return output
-        
+    
 
+class DeepBER_S_Param_Predictor(nn.Module):
+    def __init__(self, input_size, hidden, activation_fn=nn.ReLU(), batch_norm=False, dropout=0.0):
+        # Modular MLP architecture for S-Parameter prediction
+        #
+        # Args:
+        # - input_size: Number of input features
+        # - hidden: List of hidden layer sizes
+        # - activation_fn: Activation function for each of the hidden layers
+        # - batch_norm: If true batch normalization is applied after each hidden layer
+        # - dropout: Dropout rate
+        
+        super(DeepBER_S_Param_Predictor, self).__init__()
+
+        self.layers = nn.ModuleList() # create a list to hold the layers
+        current_dim = input_size # initial input dimension
+     
+        for layer in range(len(hidden)):
+            # Linear Layer
+            self.layers.append(nn.Linear(current_dim, hidden[layer]))
+
+            # Batch Normalization (optional)
+            if batch_norm:
+                self.layers.append(nn.BatchNorm1d(hidden[layer]))
+
+            # Activation Function
+            self.layers.append(activation_fn)
+
+            # Dropout (optional)
+            if dropout > 0.0:
+                self.layers.append(nn.Dropout(dropout))
+            
+            current_dim = hidden[layer] # update current dimension for next layer
+
+        # Final output layer 
+        self.output_layer = nn.Linear(current_dim, 2) # linear layer with 2 outputs [S**_real, S**_imag]
+        
+    # Forward pass
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x) # pass through each layer
+        output = self.output_layer(x) # final output layer
+        return output
 
  
