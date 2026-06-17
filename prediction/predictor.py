b7faba06d524673f88c3e3fbe57d58d5b07c5f7f
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-17 16:26:33 +0300

    cvnn predictor

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from complexNN import nn as cvnn
 
 class DeepBERPredictor(nn.Module):
     def __init__(self, input_size, hidden, activation_fn=nn.ReLU(), 
@@ -53,9 +54,9 @@ class DeepBERPredictor(nn.Module):
         return output
     
 
-class DeepBER_S_Param_Predictor(nn.Module):
+class DeepBER_Param_Predictor(nn.Module):
     def __init__(self, input_size, hidden, activation_fn=nn.ReLU(), batch_norm=False, dropout=0.0):
-        # Modular MLP architecture for S-Parameter prediction
+        # Modular MLP architecture for S-/ ABCD-Parameter prediction
         #
         # Args:
         # - input_size: Number of input features
@@ -64,7 +65,7 @@ class DeepBER_S_Param_Predictor(nn.Module):
         # - batch_norm: If true batch normalization is applied after each hidden layer
         # - dropout: Dropout rate
         
-        super(DeepBER_S_Param_Predictor, self).__init__()
+        super(DeepBER_Param_Predictor, self).__init__()
 
         self.layers = nn.ModuleList() # create a list to hold the layers
         current_dim = input_size # initial input dimension
@@ -89,6 +90,49 @@ class DeepBER_S_Param_Predictor(nn.Module):
         # Final output layer 
         self.output_layer = nn.Linear(current_dim, 2) # linear layer with 2 outputs [S**_real, S**_imag]
         
+    # Forward pass
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x) # pass through each layer
+        output = self.output_layer(x) # final output layer
+        return output
+    
+
+class DeepBER_Param_Predictor_Complex(nn.Module):
+    def __init__(self, input_size, hidden, activation_fn=nn.ReLU(), batch_norm=False, dropout=0.0):
+        # Modular Complex Valued MLP architecture for S-/ ABCD-Parameter prediction
+        #
+        # Args:
+        # - input_size: Number of input features
+        # - hidden: List of hidden layer sizes
+        # - activation_fn: Activation function for each of the hidden layers
+        # - batch_norm: If true batch normalization is applied after each hidden layer
+        # - dropout: Dropout rate
+        
+        super(DeepBER_Param_Predictor_Complex, self).__init__()
+
+        self.layers = nn.ModuleList() # create a list to hold the layers
+        current_dim = input_size # initial input dimension
+     
+        for layer in range(len(hidden)):
+            # Linear Layer
+            self.layers.append(cvnn.cLinear(current_dim, hidden[layer]))
+
+            # Batch Normalization (optional)
+            if batch_norm:
+                self.layers.append(cvnn.cBatchNorm1d(hidden[layer]))
+
+            # Activation Function
+            self.layers.append(activation_fn)
+
+            # Dropout (optional)
+            if dropout > 0.0:
+                self.layers.append(cvnn.cDropout(dropout))
+            
+            current_dim = hidden[layer] # update current dimension for next layer
+
+        # Final output layer 
+        self.output_layer = cvnn.cLinear(current_dim, 1)        
     # Forward pass
     def forward(self, x):
         for layer in self.layers:
