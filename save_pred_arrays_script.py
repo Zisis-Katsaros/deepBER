450baae2c07db24d277d445c278c7ee5935820f1
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-25 14:31:15 +0300

    save pred arrays to pt script + gitignore update

---
+++ save_pred_arrays_script.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+import os
+from load_set import create_param_prediction_arrays
+
+
+torch.manual_seed(42)
+csv_names = [
+    ["s_param_dataset_batch1.csv", "s_param_dataset_batch2.csv", "s_param_dataset_batch3.csv", "s_param_dataset_batch4.csv"],
+]
+
+test_names = ["param_prediction_test"]
+
+pred_arrays_dict = create_param_prediction_arrays(csv_names, test_names, sampling_method="lhs", subfolder="s_params")
+pred_arrays_dict_10perc = create_param_prediction_arrays(csv_names, test_names, sample_percentage=0.1, sampling_method="lhs", subfolder="s_params")
+
+
+pt_dir = "csv_files/s_params/pt"
+os.makedirs(pt_dir, exist_ok=True)
+pred_arrays_path = os.path.join(pt_dir, "pred_arrays_dict.pt")
+torch.save(pred_arrays_dict, pred_arrays_path)
+
+pred_arrays_10perc_path = os.path.join(pt_dir, "pred_arrays_dict_10perc.pt")
+torch.save(pred_arrays_dict_10perc, pred_arrays_10perc_path)
