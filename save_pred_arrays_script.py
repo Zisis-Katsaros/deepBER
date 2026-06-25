87f31d7d4ad611fb45a151005b50739d56db506f
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-26 00:54:57 +0300

    save pred arrays update

--- save_pred_arrays_script.py
+++ save_pred_arrays_script.py
@@ -11,14 +11,13 @@ csv_names = [
 
 test_names = ["param_prediction_test"]
 
-pred_arrays_dict = create_param_prediction_arrays(csv_names, test_names, sampling_method="lhs", subfolder="s_params")
-pred_arrays_dict_10perc = create_param_prediction_arrays(csv_names, test_names, sample_percentage=0.1, sampling_method="lhs", subfolder="s_params")
+
+pred_arrays_dict_30perc = create_param_prediction_arrays(csv_names, test_names, sample_percentage=0.3, sampling_method="lhs", subfolder="s_params")
 
 
 pt_dir = "csv_files/s_params/pt"
 os.makedirs(pt_dir, exist_ok=True)
-pred_arrays_path = os.path.join(pt_dir, "pred_arrays_dict.pt")
-torch.save(pred_arrays_dict, pred_arrays_path)
 
-pred_arrays_10perc_path = os.path.join(pt_dir, "pred_arrays_dict_10perc.pt")
-torch.save(pred_arrays_dict_10perc, pred_arrays_10perc_path)
+
+pred_arrays_30perc_path = os.path.join(pt_dir, "pred_arrays_dict_30perc.pt")
+torch.save(pred_arrays_dict_30perc, pred_arrays_30perc_path)
