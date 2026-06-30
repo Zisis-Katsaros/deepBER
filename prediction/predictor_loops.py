2a740dd607c81fa9e1bb4ada3615362085310848
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-30 19:15:27 +0300

    predictor loops update and refactoring

--- prediction/predictor_loops.py
+++ prediction/predictor_loops.py
@@ -1,22 +1,30 @@
 import torch
 import numpy as np
 
-def train_pred_loop(model, data, optimizer, criterion, device):
-    # Loop used for training the model for one epoch
-    #
-    # Args:
-    # - model: The neural network model to be trained
-    # - data: Training data
-    # - optimizer: Optimization algorithm 
-    # - criterion: Loss function 
-    # - device: Device to run the training on (CPU or GPU)
-    # - complex_output: Indicates if output is complex
-    # Returns:
-    # - avg_loss: Average loss over the epoch
-    # - avg_mae: Average mean absolute error over the epoch
+def train_pred_loop(model, data: torch.utils.data.DataLoader, optimizer: torch.optim.Optimizer, criterion: torch.nn.Module, device: torch.device):
+    """
+    # train_pred_loop()
+    ## Loop used for training the model for one epoch
+    
+    ## Args:
+    - model: The neural network model to be trained
+    - data: Training data
+    - optimizer: Optimization algorithm 
+    - criterion: Loss function 
+    - device: Device to run the training on (CPU or GPU)
+    - complex_output: Indicates if output is complex
+    ## Returns:
+    - avg_loss: Average loss over the epoch
+    - avg_mae: Average mean absolute error over the epoch
+    - avg_mae_per_output: Average mae of each output 
+    - avg_mae_real: Average mae of Re(Out)
+    - avg_mae_imag: Average mae of Im(Out)
+    """
 
-    model.train() # model in training mode
+    # Model in training mode
+    model.train() 
 
+    # Initialize metric sums
     total_loss = 0.0
     total_mae = 0.0 
     total_mae_real = 0.0
@@ -28,7 +36,7 @@ def train_pred_loop(model, data, optimizer, criterion, device):
     total_mae_per_output = None
     complex_outputs = None
 
-    # iterate through training batches
+    # Iterate through training batches
     for inputs, labels in data:
         inputs = inputs.to(device) 
         labels = labels.to(device)
@@ -49,13 +57,11 @@ def train_pred_loop(model, data, optimizer, criterion, device):
         # Grab info from the first batch
         if first_batch:
             num_of_outputs = outputs.size(1)
-            complex_outputs = outputs.is_complex()
-            
+            complex_outputs = outputs.is_complex()   
             if complex_outputs:
                 total_mae_per_output = torch.zeros([num_of_outputs,2], device=device)
             else:
                 total_mae_per_output = torch.zeros(num_of_outputs, device=device)
-            
             first_batch = False
 
         total_loss += loss.item() * inputs.size(0) # accumulate loss
@@ -89,23 +95,34 @@ def train_pred_loop(model, data, optimizer, criterion, device):
 
     return avg_loss, avg_mae, avg_mae_per_output, avg_mae_real, avg_mae_imag # return loss and MAE
 
-# Test Loop:
-def test_pred_loop(model, data, criterion, device):
-    # Loop used for evaluating the model
-    # 
-    # Args:
-    # - model: The neural network model to be evaluated
-    # - data: Validation or Test data
-    # - criterion: Loss function
-    # - device: Device to run the evaluation on (CPU or GPU)
-    # Returns:
-    # - avg_loss: Average loss over the evaluation
-    # - avg_mae: Average mean absolute error over the evaluation
-    # - all_preds: All predictions made by the model
-    # - all_labels: All true labels corresponding to the predictions
 
-    model.eval() # model in evaluation mode
+def test_pred_loop(model, data: torch.utils.data.DataLoader, criterion: torch.nn.Module, device: torch.device):
+    """
+    # test_pred_loop()
+    ## Loop used for evaluating the model
+    
+    ## Args:
+    - model: The neural network model to be evaluated
+    - data: Validation or Test data
+    - criterion: Loss function
+    - device: Device to run the evaluation on (CPU or GPU)
+    ## Returns:
+    - avg_loss: Average loss over the evaluation
+    - avg_mae: Average mean absolute error over the evaluation
+    - all_preds: All predictions made by the model
+    - all_labels: All true labels corresponding to the predictions
+    - avg_mae_per_output: Average mae for each output
+    - avg_mape_per_output: Average mape for each output
+    - avg_mae_real: Average mae for Re(Out)
+    - avg_mae_imag: Average mae for Im(Out)
+    - avg_mape_real: Average mape for Re(Out)
+    - avg_mape_imag: Average mape for Im(Out)
+    """
 
+    # Model in evaluation mode
+    model.eval() 
+
+    # Initialize metric sums
     total_loss = 0.0
     total_mae = 0.0 
     total_mae_real = 0.0
@@ -125,10 +142,10 @@ def test_pred_loop(model, data, criterion, device):
     all_preds = []
     all_labels = []
 
-    eps = 1e-15 
+    eps = 1e-8 # to account for labels being equal to 0
 
     with torch.no_grad(): # disable gradient calculation
-        for inputs, labels in data:
+        for inputs, labels, _ in data:
             inputs = inputs.to(device)
             labels = labels.to(device)
 
@@ -189,8 +206,6 @@ def test_pred_loop(model, data, criterion, device):
                 total_mape += torch.sum(error / (torch.abs(labels) + eps)).item()
 
                 total_mae_per_output += torch.sum(error, dim=0)
-
-
 
     avg_loss = total_loss / total_samples
 
