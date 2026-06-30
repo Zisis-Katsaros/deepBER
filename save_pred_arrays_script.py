ee8754b7decf2b2743a35be713db82fa55bd28d5
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-30 17:07:55 +0300

    svae pred arrays script update to fit the new create pred arrays implementation

--- save_pred_arrays_script.py
+++ save_pred_arrays_script.py
@@ -1,23 +1,25 @@
 import torch
-import numpy as np
 import os
 from load_set import create_param_prediction_arrays
 
-
 torch.manual_seed(42)
-csv_names = [
-    ["s_param_dataset_batch1.csv", "s_param_dataset_batch2.csv", "s_param_dataset_batch3.csv", "s_param_dataset_batch4.csv"],
-]
+csv_names = ["s_param_dataset_batch1.csv", "s_param_dataset_batch2.csv", "s_param_dataset_batch3.csv", "s_param_dataset_batch4.csv"]
 
-test_names = ["param_prediction_test"]
+x_array, s_dict, a_dict, b_dict, c_dict, d_dict, feature_columns = create_param_prediction_arrays(csv_names, sample_percentage=1.0, sampling_method="lhs", subfolder="s_params")
 
-
-pred_arrays_dict_30perc = create_param_prediction_arrays(csv_names, test_names, sample_percentage=0.3, sampling_method="lhs", subfolder="s_params")
-
+pred_arrays_dict = {
+    "x_array": x_array,
+    "s_dict": s_dict,
+    "a_dict": a_dict,
+    "b_dict": b_dict,
+    "c_dict": c_dict,
+    "d_dict": d_dict,
+    "feature_columns": feature_columns
+}
 
 pt_dir = "csv_files/s_params/pt"
 os.makedirs(pt_dir, exist_ok=True)
 
 
-pred_arrays_30perc_path = os.path.join(pt_dir, "pred_arrays_dict_30perc.pt")
-torch.save(pred_arrays_dict_30perc, pred_arrays_30perc_path)
+pred_arrays_path = os.path.join(pt_dir, "pred_arrays_dict.pt")
+torch.save(pred_arrays_dict, pred_arrays_path)
