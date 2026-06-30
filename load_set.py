022166bf1b9426ced89d3630455c7f9d14213313
Zisis Katsaros <katsaros.z@outlook.com>
2026-06-30 14:20:34 +0300

    create_param_prediction_arrays to work only for single "test" and sample_id free update

--- load_set.py
+++ load_set.py
@@ -6,70 +6,25 @@ from torch.utils.data import DataLoader, TensorDataset, random_split
 from dataset_manipulation import extend_features, exclude_columns
 from classification.ber_to_class import ber_to_class
 from prediction.s2abcd import s2generalized_abcd
+from lhs import latin_hypercube_order
+from typing import Literal
 
+def get_grouping(x_array, n_non_unique_feats: int = 7, round_decimals: int = 5):
+	geometries = x_array[:, :n_non_unique_feats]
 
-def latin_hypercube_order(x_array, sample_size, seed=42, grouped=False):
-	# Builds a space-filling ordering of sample indices using Latin Hypercube Sampling
-	#
-	# Args:
-	# - x_array: samples
-	# - sample_size: number of samples to be selected
-	# - seed: Random seed for reproducibility
-	# - grouped: Whether samples are grouped under sample_ids and either inclusion or exclusion of the whole group is 
-	# 			required  
-	# Returns:
-	# selected_indices: indices of selected samples
+	if round_decimals is not None:
+		geometries = np.round(geometries, decimals=round_decimals)
 
-	x_view = np.asarray(x_array)
+	unique_geoms, inverse_indices = np.unique(geometries, axis=0, return_inverse=True)
 
-	if grouped:
-        # Isolate the unique geometry IDs and grab the index of their first occurrence
-		unique_ids, first_indices = np.unique(x_view[:, 0], return_index=True)
-        
-		features_matrix = x_view[first_indices, 1:-1].astype(float)
-		id_mapping = unique_ids
-	else:
-		features_matrix = x_view
-		id_mapping = np.arange(len(x_view))
+	sort_idx = np.argsort(inverse_indices)
+	sorted_group_ids = inverse_indices[sort_idx]
 
-	total_size = len(features_matrix)
-	if sample_size > total_size:
-		raise ValueError("sample_size cannot exceed the number of available samples.")
+	# Find the boundries where group ID changes
+	split_indices = np.flatnonzero(np.diff(sorted_group_ids)) + 1
 
-	if sample_size <= 0:
-		raise ValueError("sample_size must be greater than 0.")
-
-	
-	x_min = features_matrix.min(axis=0)
-	x_max = features_matrix.max(axis=0)
-	x_range = np.where((x_max - x_min) == 0.0, 1.0, x_max - x_min)
-	x_norm = (features_matrix - x_min) / x_range
-
-	# Build LHS points
-	rng = np.random.default_rng(seed)
-	num_of_features = x_norm.shape[1]
-	lhs_points = np.empty((sample_size, num_of_features))
-
-	for feature_idx in range(num_of_features):
-		permutation = rng.permutation(sample_size)
-		lhs_points[:, feature_idx] = (permutation + rng.random(sample_size)) / sample_size
-
-	selected_indices = []
-	available_mask = np.ones(total_size, dtype=bool)
-
-	# For each LHS point, find the closest available sample in the normalized feature space and select it
-	for point in lhs_points:
-		available_indices = np.flatnonzero(available_mask)
-		available_points = x_norm[available_mask]
-
-		distances = np.sum((available_points - point) ** 2, axis=1)
-		chosen_local_index = int(np.argmin(distances))
-		chosen_global_index = int(available_indices[chosen_local_index])
-
-		selected_indices.append(id_mapping[chosen_global_index])
-		available_mask[chosen_global_index] = False
-
-	return np.asarray(selected_indices)
+	grouping_indices = np.split(sort_idx, split_indices)
+	return grouping_indices, unique_geoms
 
 
 def create_arrays(csv_names, target_columns, thresholds, test_names, manipulate_features = None,  
@@ -136,138 +91,123 @@ def create_arrays(csv_names, target_columns, thresholds, test_names, manipulate_
 	return test_info_dict
 
 
-def create_param_prediction_arrays(csv_names, test_names, expected_ports = None, target_columns = None, 
-								   manipulate_features = None, sample_percentage=1.0, seed=42, 
-								   sampling_method="random", subfolder=None):
-	# Creates arrays for each test dataset
-	#
-	# Args:
-	# - csv_names: List of lists of CSV file names for each test
-	# - test_names: List of test names for printing results
-	# - expected_ports: List of expected number of ports for each test
-	# - target_columns: List of target column names for each test
-	# - manipulate_features: List of booleans indicating whether to apply feature manipulation for each test
-	# - sampling_method: "random" or "lhs" for subsampling the loaded dataset
-	# - subfolder: Subfolder in csv_files where the datasets are located
-	# Returns:
-	# - test_info_dict: Dictionary with test names as keys and tuples (x_array, s_dict, a_dict, b_dict, c_dict, 
-	# 					d_dict, feature_columns) as values
+def create_param_prediction_arrays(csv_names: list[str], expected_ports:int = 18, target_columns: list[str] = [], 
+								   manipulate_features: bool = True, sample_percentage: float = 1.0, seed: int = 42, 
+								   sampling_method: Literal["random", "lhs"] = "random", subfolder: str = None):
+	"""
+	# create_param_prediction_arrays()
+	## Creates arrays features and labels arrays from given csv file(s)
 	
-	# Setting up default arguments
-	if expected_ports is None:
-		expected_ports = [18] * len(csv_names)
-	elif len(expected_ports) != len(csv_names):
-		raise ValueError("Length of expected_ports must match length of csv_names.")
+	## Args:
+	- csv_names: List of CSV file names as they appear inside csv_files/ 
+	- expected_ports: Expected number of ports for the equivalent circuit model 
+	- target_columns: List of target column names
+	- manipulate_features: Whether to apply feature manipulation (adds width to space ratio, cross sectional area and gnd width to width ratio)
+	- sampling_method: "random" or "lhs" for subsampling the loaded dataset
+	- subfolder: Subfolder in csv_files/ where the datasets are located
+	## Returns:
+	- Tuple (x_array, s_dict, a_dict, b_dict, c_dict, d_dict, feature_columns)
+	"""
 
-	if target_columns is None:
-		target_columns = []
-		for idx in range(len(expected_ports)):
-			current_test_columns = []
-			for i in range(expected_ports[idx]):
-				for j in range(i, expected_ports[idx]):
+	# Setting up default target columns
+	if target_columns == []:
+			for i in range(expected_ports):
+				for j in range(i, expected_ports):
 					target_columns.append(f"S{i+1}{j+1}_real")
 					target_columns.append(f"S{i+1}{j+1}_imag")
 
+	# Extract inputs, labels and feature names from csv files
+	x_array, s_array, feature_columns = load_csv_dataset(csv_names, target_columns=target_columns, subfolder=subfolder)
 
-	if manipulate_features is None:
-		manipulate_features = [True] * len(csv_names)
-	elif len(manipulate_features) != len(csv_names):
-		raise ValueError("Length of manipulate_features must match length of csv_names.")
+	num_of_samples = len(s_array)	
+
+	if manipulate_features:
+		x_array, feature_columns = extend_features(x_array, feature_columns, "width", "space", "/", "width_space_ratio")
+		x_array, feature_columns = extend_features(x_array, feature_columns, "width", "metal_thickness", "*", "cross_sectional_area")
+		x_array, feature_columns = extend_features(x_array, feature_columns, "gnd_width", "width", "/", "gnd_width_width_ratio")
 	
+	s_dict = {}
+	s_dict["all"] = s_array
 
-	test_info_dict = {}
-	for idx, csv_batch in enumerate(csv_names):
-		x_array, s_array, feature_columns = load_csv_dataset(csv_batch, target_columns=target_columns, 
-													   subfolder=subfolder)
+	# Initialize S-matrices
+	s_matrices = np.zeros((num_of_samples, expected_ports, expected_ports), dtype=complex)
 
-		num_of_samples = len(s_array)	
+	# Create S dictionary (keys: "S11", "S12"... "Sij"... "SNN", values: Sij_labels, main diagonal and upper triangle only)
+	# s_array to NxN S-matrix, where N=expected ports
+	col_idx = 0
+	for i in range(expected_ports):
+		for j in range(i, expected_ports):
+			Sij_real = s_array[:, col_idx]
+			Sij_imag = s_array[:, col_idx + 1]
 
-		if manipulate_features[idx]:
-			x_array, feature_columns = extend_features(x_array, feature_columns, "width", "space", "/", "width_space_ratio")
-			x_array, feature_columns = extend_features(x_array, feature_columns, "width", "metal_thickness", "*", "cross_sectional_area")
-			x_array, feature_columns = extend_features(x_array, feature_columns, "gnd_width", "width", "/", "gnd_width_width_ratio")
+			Sij_complex = Sij_real + 1j * Sij_imag
+
+			key = f"S{i+1}{j+1}"
+			s_dict[key] = Sij_complex
+
+			s_matrices[:, i, j] = Sij_complex
+			s_matrices[:, j, i] = Sij_complex
+
+			# Move to next column
+			col_idx +=  2
+
+	# Calculate ABCD parameters from S-matrix
+	A, B, C, D = s2generalized_abcd(s_matrices)
+
+	# Create ABCD dictionaries
+	a_dict = {}
+	b_dict = {}
+	c_dict = {}
+	d_dict = {}
+	col_idx = 0
+	for submatrices, dict, name in zip([A,B,C,D], [a_dict, b_dict, c_dict, d_dict], ["A", "B", "C", "D"]):
+		# Dictionary entry with all elements of the ABCD matrices together 
+		MATflat = submatrices.reshape(num_of_samples, submatrices.shape[1]**2)
+
+		all_array = np.empty((num_of_samples, 2*submatrices.shape[1]**2))
+		all_array[:, 0::2] = MATflat.real
+		all_array[:, 1::2] = MATflat.imag
+
+		dict["all"] = all_array
+
+		# Dictionary entries for each element of the ABCD matrices
+		for i in range(submatrices.shape[1]):
+			for j in range(submatrices.shape[2]):
+				MATij = submatrices[:, i, j]
+
+				key = f"{name}{i+1}{j+1}"
+				dict[key] = MATij	
+
+	# Selecting a percentage of the total dataset
+	if not 0.0 < sample_percentage <= 1.0:
+		raise ValueError("sample_percentage must be within the interval (0.0, 1.0].")
+
+	grouping, unique_geoms = get_grouping(x_array)
 	
+	total_size = len(grouping)
+	sample_size = int(total_size * sample_percentage)
+	sample_size = min(total_size, max(1, sample_size))
 
-		s_dict = {}
-		s_dict["all"] = s_array
+	if sampling_method == "random":
+		generator = torch.Generator().manual_seed(seed)
+		sampled_group_idxs = torch.randperm(total_size, generator=generator)[:sample_size].numpy()
+	elif sampling_method == "lhs":
+		selected_group_ids = latin_hypercube_order(unique_geoms, sample_size, seed=seed, grouped=True)
+	else:
+		raise ValueError("sampling_method must be 'random' or 'lhs'.")
+	
+	# Get indices of selected rows in x_array and create sampling mask
+	selected_row_indices = np.concatenate([grouping[i] for i in sampled_group_idxs])
+	mask = np.isin(x_array[:,0], selected_row_indices)
 
-		# s matrices for calculating abcd parameter matrices
-		s_matrices = np.zeros((num_of_samples, expected_ports[idx], expected_ports[idx]), dtype=complex)
+	x_array = x_array[mask]
 
-		col_idx = 0
-		for i in range(expected_ports[idx]):
-			for j in range(i, expected_ports[idx]):
-				Sij_real = s_array[:, col_idx]
-				Sij_imag = s_array[:, col_idx + 1]
-
-				Sij_complex = Sij_real + 1j * Sij_imag
-
-				key = f"S{i+1}{j+1}"
-				s_dict[key] = Sij_complex
-
-				s_matrices[:, i, j] = Sij_complex
-				s_matrices[:, j, i] = Sij_complex
-
-				# Move to next column
-				col_idx +=  2
-
-		A, B, C, D = s2generalized_abcd(s_matrices)
-
-
-		a_dict = {}
-		b_dict = {}
-		c_dict = {}
-		d_dict = {}
-		col_idx = 0
-		for submatrices, dict, name in zip([A,B,C,D], [a_dict, b_dict, c_dict, d_dict], ["A", "B", "C", "D"]):
-			# Dictionary entry with all elements of the ABCD matrices together 
-			MATflat = submatrices.reshape(num_of_samples, submatrices.shape[1]**2)
-
-			all_array = np.empty((num_of_samples, 2*submatrices.shape[1]**2))
-			all_array[:, 0::2] = MATflat.real
-			all_array[:, 1::2] = MATflat.imag
-
-			dict["all"] = all_array
-
-			# Dictionary entries for each element of the ABCD matrices
-			for i in range(submatrices.shape[1]):
-				for j in range(submatrices.shape[2]):
-					MATij = submatrices[:, i, j]
-
-					key = f"{name}{i+1}{j+1}"
-					dict[key] = MATij	
-
-		# Selecting a percentage of the total dataset
-		if not 0.0 < sample_percentage <= 1.0:
-			raise ValueError("sample_percentage must be within the interval (0.0, 1.0].")
-
-		sample_ids = list(set(x_array[:,0]))
-
-		total_size = len(sample_ids)
-		sample_size = int(total_size * sample_percentage)
-		sample_size = min(total_size, max(1, sample_size))
-
-		if sampling_method == "random":
-			generator = torch.Generator().manual_seed(seed)
-			sampled_idxs = torch.randperm(total_size, generator=generator)[:sample_size].numpy()
-			selected_ids = sample_ids[sampled_idxs]
-		elif sampling_method == "lhs":
-			selected_ids = latin_hypercube_order(x_array, sample_size, seed=seed, grouped=True)
-		else:
-			raise ValueError("sampling_method must be 'random' or 'lhs'.")
-
-		mask = np.isin(x_array[:,0], selected_ids)
-
-		x_array = x_array[mask]
-
-		for dict in [s_dict, a_dict, b_dict, c_dict, d_dict]:
-			for key in dict.keys():
-				dict[key] = dict[key][mask]
+	for dict in [s_dict, a_dict, b_dict, c_dict, d_dict]:
+		for key in dict.keys():
+			dict[key] = dict[key][mask]
 			
-
-		# Return
-		test_info_dict[test_names[idx]] = (x_array, s_dict, a_dict, b_dict, c_dict, d_dict, feature_columns)
-	return test_info_dict
+	# Return
+	return x_array, s_dict, a_dict, b_dict, c_dict, d_dict, feature_columns
 
 
 def load_csv_dataset(csv_names, target_columns="BER", subfolder=None):
@@ -483,7 +423,7 @@ def create_param_dataloader(
 	# - standard_scale: If true standard scaling is applied to features
 	# - split_method: "random" or "lhs" for splitting the dataset
 	# Returns:
-	# - dataloader: [train_data, val_data, test_data] 
+	# - dataloader: [train_data, val_data, test_data], x_scale_params, y_scale_params
 
 
 	if len(y_array) == 0:
@@ -521,6 +461,9 @@ def create_param_dataloader(
 	val_idx = np.isin(x_array[:,0], val_ids)
 	test_idx = np.isin(x_array[:,0], test_ids)
 	
+	str_id_array = x_array[:, 0]
+	unique_string_ids, id_array_int = np.unique(str_id_array, return_inverse=True)
+
 	x_features = x_array[:, 1:].astype(float)
 	# Standard scaling
 	if standard_scale:
@@ -538,21 +481,31 @@ def create_param_dataloader(
 	train_set = TensorDataset(
 		torch.from_numpy(x_features[train_idx]),
 		torch.from_numpy(y_array[train_idx]),
+		torch.from_numpy(id_array_int[train_idx])
 	)
 	val_set = TensorDataset(
 		torch.from_numpy(x_features[val_idx]),
 		torch.from_numpy(y_array[val_idx]),
+		torch.from_numpy(id_array_int[val_idx])
 	)
 	test_set = TensorDataset(
 		torch.from_numpy(x_features[test_idx]),
 		torch.from_numpy(y_array[test_idx]),
+		torch.from_numpy(id_array_int[test_idx])
 	)
 
 	train_data = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 	val_data = DataLoader(val_set, batch_size=batch_size, shuffle=False)
 	test_data = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
+	sample_ids = {
+		"train": train_ids,
+        "val": val_ids,
+        "test": test_ids,
+		"id_mapping": unique_string_ids
+	}
+
 	if standard_scale:
-		return [train_data, val_data, test_data], (y_train_mean, y_train_std)
+		return [train_data, val_data, test_data], (x_train_mean, x_train_std), (y_train_mean, y_train_std), sample_ids
 	else:
-		return [train_data, val_data, test_data]
+		return [train_data, val_data, test_data], 
