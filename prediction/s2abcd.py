e64856dd67e74cb1be03a895f35d211582b81d29
Zisis Katsaros <katsaros.z@outlook.com>
2026-07-03 16:02:30 +0300

    s_dict2mat + trans_param_mat2dict + s2abcd_dict functions

--- prediction/s2abcd.py
+++ prediction/s2abcd.py
@@ -30,3 +30,44 @@ def s2generalized_abcd(s, z0=50.0):
     D = 0.5 * ((I - s11) @ s21_inv @ (I + s22) + s12)
 
     return A, B, C, D
+
+def s_dict2mat(s_dict, expected_ports=18):
+    first_val = next(iter(s_dict.values()))
+    num_samples = len(first_val)
+    s_matrices = np.zeros((num_samples, expected_ports, expected_ports), dtype=np.complex64)
+
+    for i in range(expected_ports):
+        for j in range(i, expected_ports):
+            key = f"S{i+1}{j+1}"
+            if np.iscomplexobj(s_dict[key]):
+                Sij = s_dict[key]
+            else:
+                Sij_real = s_dict[key][:, 0]
+                Sij_imag = s_dict[key][:, 1]
+                Sij = Sij_real + 1j * Sij_imag
+
+            s_matrices[:, i, j] = Sij
+            s_matrices[:, j, i] = Sij
+    return s_matrices
+
+
+def trans_param_mat2dict(matrices, prefix):
+    out_dict = {}
+    for i in range(matrices.shape[1]):
+        for j in range(matrices.shape[2]):
+            MATij = matrices[:, i, j]
+            
+            key = f"{prefix}{i+1}{j+1}"
+            out_dict[key] = MATij
+    return out_dict
+
+
+def s2abcd_dict(s_dict, expected_ports=18, z0=50.0):
+    s_matrices = s_dict2mat(s_dict, expected_ports=expected_ports)
+    A, B, C, D = s2generalized_abcd(s_matrices, z0=z0)
+
+    a_dict = trans_param_mat2dict(A, "A")
+    b_dict = trans_param_mat2dict(B, "B")
+    c_dict = trans_param_mat2dict(C, "C")
+    d_dict = trans_param_mat2dict(D, "D")
+    return a_dict, b_dict, c_dict, d_dict
