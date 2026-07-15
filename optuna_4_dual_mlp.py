0ed850b8ebc298c188d763454409c3bdee7c885c
Zisis Katsaros <katsaros.z@outlook.com>
2026-07-15 17:14:44 +0300

    dual_mlp script update to train on 50% of the dataset so that the rest can be used for physics enforcing

--- optuna_4_dual_mlp.py
+++ optuna_4_dual_mlp.py
@@ -3,25 +3,33 @@ from torch import nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from prediction.param_pred_optuna import run_optuna
 from load_set import create_param_dataloader
+from dataset_splitting import split_dataset
 from prediction.predictor import DeepBER_Param_Predictor
 from rmse import RMSELoss
 from prediction.test_predictor_config import test_predictor_configuration, single_geometry_test, abcd_preds_vs_act_freq
 import numpy as np
-from dataset_manipulation import pki_extend
 from export_files_for_transient import export_files_for_transient
 
 
+
 # ============================================= Initializing Dataset ============================================= #
-torch.manual_seed(42)
+seed = 42
+torch.manual_seed(seed)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-pki = False
 pred_arrays_dict = torch.load("csv_files/s_params/pt/pred_arrays_dict.pt", weights_only=False)
-s_mock_dict = torch.load("csv_files/s_params/pt/s_mock_dict.pt", weights_only=False)
 
 x_array = pred_arrays_dict["x_array"].astype(np.float32)
 s_dict = pred_arrays_dict["s_dict"]
 feature_columns = pred_arrays_dict["feature_columns"]
+
+# Split the dataset into two parts: one for s-parameter (per-element) prediction and one for post-prediction physics enforcing
+x_array_pred, pred_row_indices = split_dataset(x_array, sample_percentage=0.5, sampling_method="lhs", seed=seed)
+s_dict_pred = {key: s_dict[key][pred_row_indices] for key in s_dict.keys()}
+
+# physics_row_indices = [idx for idx in range(x_array.shape[0]) if idx not in pred_row_indices]
+# x_array_physics = x_array[physics_row_indices]
+# s_dict_physics = {key: s_dict[key][physics_row_indices] for key in s_dict.keys()}
 
 """
 hidden_map = {
@@ -41,21 +49,18 @@ labels_dict_per_geom = np.array([{} for _ in range(geoms_tested)], dtype=dict)
 preds_dict_per_geom = np.array([{} for _ in range(geoms_tested)], dtype=dict)
 freq_arrays_per_geom = [None for _ in range(geoms_tested)]
 
-processed_elements = [key for idx, key in enumerate(s_dict.keys()) if idx < 200 and key != "all"] 
-elements = list(key for key in s_dict.keys() if key != "all") # ["S55", "S78", "S217"]
+processed_elements = [key for idx, key in enumerate(s_dict.keys()) if idx < -1 and key != "all"] 
+elements = list(key for key in s_dict.keys() if key != "all")
 for element in elements:
     if element in processed_elements:
         continue
     print(f"Training and testing for {element}\n")
-    if pki:
-        x_xtnd, feature_columns_xtnd = pki_extend(x_array, feature_columns, s_mock_dict[element])
-    else:
-        x_xtnd = x_array
-    y_array = np.stack([s_dict[element].real, s_dict[element].imag], axis=1)
+    
+    y_array = np.stack([s_dict_pred[element].real, s_dict_pred[element].imag], axis=1)
     out_size = y_array.shape[1] if y_array.ndim > 1 else 1
 
     dataloader, x_scale_params, y_scale_params = create_param_dataloader(
-                    x_xtnd,
+                    x_array_pred,
                     y_array,
                     batch_size=128,
                     seed=42,
@@ -87,28 +92,23 @@ for element in elements:
     criterion=criterion,
     optimizer=optimizer,
     scheduler=scheduler,
-    epochs=300,
+    epochs=1,
     early_stopping=True,
     patience=10,
     y_scale_params=y_scale_params,
     training_curves=True,
     predicted_vs_actual=True,
-    test_out_dir = f"out_files/dual_mlp/{element}/pki" if pki else f"out_files/dual_mlp/{element}/no_pki",
+    test_out_dir = f"out_files/dual_mlp",
     close_figures=True
     )
 
-# """
+"""
 for element in elements:
-    if pki:
-        x_xtnd, feature_columns_xtnd = pki_extend(x_array, feature_columns, s_mock_dict[element])
-    else:
-        x_xtnd = x_array
-        feature_columns_xtnd = feature_columns
     y_array = np.stack([s_dict[element].real, s_dict[element].imag], axis=1)
     out_size = y_array.shape[1] if y_array.ndim > 1 else 1
 
     dataloader, x_scale_params, y_scale_params = create_param_dataloader(
-                    x_xtnd,
+                    x_array,
                     y_array,
                     batch_size=128,
                     seed=42,
@@ -117,7 +117,7 @@ for element in elements:
                     )
     
     predictor = DeepBER_Param_Predictor(
-        input_size=len(feature_columns_xtnd), 
+        input_size=len(feature_columns), 
         hidden=[48, 64, 96, 64, 48], 
         activation_fn=nn.GELU(), 
         output_size=out_size,
@@ -125,7 +125,7 @@ for element in elements:
         ).to(device) 
     predictor.load_state_dict(torch.load(f"out_files/dual_mlp/{element}/pki/best_model.pth" if pki else f"out_files/dual_mlp/{element}/no_pki/best_model.pth", map_location=device))
 # """
-    
+""" 
     geometries, labels_per_geom, preds_per_geom, freq_arrays = single_geometry_test(
         title=f"{element}",
         device=device,
@@ -146,7 +146,7 @@ for element in elements:
             freq_arrays_per_geom[geom_idx] = geom_freq_array
 
 export_files_for_transient(geometries, feature_columns, labels_dict_per_geom, preds_dict_per_geom, freq_arrays_per_geom, save_dir="csv_files/export4transient")
-
+"""
 
 
 
--- save_pred_arrays_script.py
+++ save_pred_arrays_script.py
@@ -5,7 +5,7 @@ from load_set import create_param_prediction_arrays
 torch.manual_seed(42)
 csv_names = ["s_param_dataset_batch1.csv", "s_param_dataset_batch2.csv", "s_param_dataset_batch3.csv", "s_param_dataset_batch4.csv"]
 
-x_array, s_dict, a_dict, b_dict, c_dict, d_dict, feature_columns = create_param_prediction_arrays(csv_names, sample_percentage=1.0, sampling_method="lhs", subfolder="s_params")
+x_array, s_dict, a_dict, b_dict, c_dict, d_dict, feature_columns = create_param_prediction_arrays(csv_names, sample_percentage=0.5, sampling_method="lhs", subfolder="s_params")
 
 pred_arrays_dict = {
     "x_array": x_array,
@@ -21,5 +21,5 @@ pt_dir = "csv_files/s_params/pt"
 os.makedirs(pt_dir, exist_ok=True)
 
 
-pred_arrays_path = os.path.join(pt_dir, "pred_arrays_dict.pt")
+pred_arrays_path = os.path.join(pt_dir, "pred_arrays_dict_50perc.pt")
 torch.save(pred_arrays_dict, pred_arrays_path)
