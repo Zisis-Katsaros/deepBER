7b832f4569b8d8ddfd235e520428f5e074edc435
Zisis Katsaros <katsaros.z@outlook.com>
2026-07-19 17:03:36 +0300

    Update transient_evaluation_script.m

--- transient_evaluation_script.m
+++ transient_evaluation_script.m
@@ -3,7 +3,7 @@ addpath('transient');
 start_geom = 1;
 max_geoms = 2;
 for geom_idx = start_geom:max_geoms
-    filename_preds = ['csv_files/export4transient/preds/geom', num2str(geom_idx), '_pred.s18p'];
-    filename_actuals = ['csv_files/export4transient/actuals/geom', num2str(geom_idx), '_actual.s18p'];
+    filename_preds = ['out_files/pi_stcnn/touchstone_files/preds/geom', num2str(geom_idx), '_pred.s18p']; %['csv_files/export4transient/preds/geom', num2str(geom_idx), '_pred.s18p'];
+    filename_actuals = ['out_files/pi_stcnn/touchstone_files/actuals/geom', num2str(geom_idx), '_actual.s18p']; %['csv_files/export4transient/actuals/geom', num2str(geom_idx), '_actual.s18p'];
     run_transient_evaluation(filename_preds, filename_actuals, sprintf('Geometry %d', geom_idx));
 end
