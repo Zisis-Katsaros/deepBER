de8d5c8a3a24eac5d2f6f262791960a8581e24c7
Zisis Katsaros <katsaros.z@outlook.com>
2026-07-20 13:04:07 +0300

    create_param_dataloader updated standard scale argument to (scale feats, scale labels) touple and PI-sTCNN test with unscaled labels

--- transient_evaluation_script.m
+++ transient_evaluation_script.m
@@ -1,9 +1,9 @@
 addpath('transient');
 
-start_geom = 7;
+start_geom = 1;
 max_geoms = 2;
 for geom_idx = start_geom:(start_geom + max_geoms - 1)
-    filename_preds = ['out_files/pi_stcnn/touchstone_files_2/preds/geom', num2str(geom_idx), '_pred.s18p']; %['csv_files/export4transient/preds/geom', num2str(geom_idx), '_pred.s18p'];
-    filename_actuals = ['out_files/pi_stcnn/touchstone_files_2/actuals/geom', num2str(geom_idx), '_actual.s18p']; %['csv_files/export4transient/actuals/geom', num2str(geom_idx), '_actual.s18p'];
+    filename_preds = ['out_files/pi_stcnn/touchstone_files_3/preds/geom', num2str(geom_idx), '_pred.s18p']; %['csv_files/export4transient/preds/geom', num2str(geom_idx), '_pred.s18p'];
+    filename_actuals = ['out_files/pi_stcnn/touchstone_files_3/actuals/geom', num2str(geom_idx), '_actual.s18p']; %['csv_files/export4transient/actuals/geom', num2str(geom_idx), '_actual.s18p'];
     run_transient_evaluation(filename_preds, filename_actuals, sprintf('Geometry %d', geom_idx));
 end
