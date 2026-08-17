89d31c191254ceb2f34e41842a7b116c8e44cfdd
Zisis Katsaros <katsaros.z@outlook.com>
2026-08-17 15:46:08 +0300

    PI-sTCNN to run optuna on HPC

--- pi_stcnn_script.py
+++ pi_stcnn_script.py
@@ -35,10 +35,10 @@ if pki:
 else:
     x_array, feature_columns, y_array = organize_dataset_for_pi_stcnn(x_array, s_dict, feature_columns)
 
-"""
-db_path = "out_files/pi_stcnn/pi_stcnn_study.db"
+#"""
+db_path = "out_files/pi_stcnn/pi_stcnn_study2.db"
 storage_url = f"sqlite:///{db_path}"
-run_pi_stcnn_optuna(x_array, y_array, feature_columns, n_trials=175, n_epochs=600, storage=storage_url)
+run_pi_stcnn_optuna(x_array, y_array, feature_columns, n_trials=400, n_epochs=450, storage=storage_url)
 
 """
 dataloader, x_scale_params, y_scale_params, y_weights = create_param_dataloader(
@@ -118,3 +118,4 @@ export_files_for_transient(
     freq_arrays_per_geom=freq_arrays_per_geom,
     save_dir="out_files/pi_stcnn/touchstone_files"
 )
+"""
